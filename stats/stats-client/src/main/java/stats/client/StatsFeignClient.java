a49a9809852c34473db3828339de74c504e1496e
Sergey Samoylov <sam0lov@yandex.ru>
2026-02-28 12:14:47 +0300

    Fix StatsFeignClient

--- stats/stats-client/src/main/java/stats/client/StatsFeignClient.java
+++ stats/stats-client/src/main/java/stats/client/StatsFeignClient.java
@@ -4,7 +4,6 @@ import dto.GetStatsDto;
 import dto.SaveHitDto;
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +14,10 @@ import java.util.List;
 @FeignClient(name = "stats-server")
 public interface StatsFeignClient {
 
-    @PostMapping(value = "/hit", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping("/hit")
     void saveStats(@RequestBody @Valid SaveHitDto requestBody);
 
-    @GetMapping(value = "/stats", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping("/stats")
     List<GetStatsDto> getStats(@RequestParam("start") String start,
                                @RequestParam("end") String end,
                                @RequestParam(value = "uris", required = false) List<String> uris,
