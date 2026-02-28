1643aabc9b46077ee262e8709e0cd9e26ae8c9d4
Sergey Samoylov <sam0lov@yandex.ru>
2026-02-28 12:12:23 +0300

    Implemented StatsFeignClient

--- core/main-service/src/main/java/ewm/MainServiceApplication.java
+++ core/main-service/src/main/java/ewm/MainServiceApplication.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "stats.client")
 @ComponentScan({
         "ewm",
         "stats.client"
